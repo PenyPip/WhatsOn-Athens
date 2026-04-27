e07c44a0112d9238a9a5eab4ba8eb5228974798d
PenyPip <penynikolaidou@gmail.com>
2026-04-27 19:30:57 +0300

    strapi

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://the37n.gr'),
   app: {
     keys: env.array('APP_KEYS'),
+  },
+  admin: {
+    allowedHosts: ['the37n.gr', 'www.the37n.gr'],
   },
 });
