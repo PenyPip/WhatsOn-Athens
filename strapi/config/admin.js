704a02bc97429ec0a45825237fa761c28a5605d3
PenyPip <penynikolaidou@gmail.com>
2026-04-27 21:40:37 +0300

    admin

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'whatson-admin-jwt-secret'),
+  auth: {
+    secret: env('ADMIN_JWT_SECRET', 'whatson-admin-jwt-secret'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT', 'whatson-api-token-salt'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT', 'whatson-transfer-token-salt'),
     },
-    apiToken: {
-      salt: env('API_TOKEN_SALT', 'whatson-api-token-salt'),
-    },
-    transfer: {
-      token: {
-        salt: env('TRANSFER_TOKEN_SALT', 'whatson-transfer-token-salt'),
-      },
-    },
-  });
+  },
+  secrets: {
+    encryptionKey: env('ENCRYPTION_KEY', 'whatson-encryption-key-32chars!!'),
+  },
+});
