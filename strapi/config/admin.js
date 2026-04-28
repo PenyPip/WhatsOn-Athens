e03f20227e3524d55202fcbdfd8b01eb8541e394
PenyPip <penynikolaidou@gmail.com>
2026-04-28 17:21:33 +0300

    debug

--- strapi/config/admin.js
+++ strapi/config/admin.js
@@ -1,13 +1,13 @@
 module.exports = ({ env }) => ({
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'whatson-admin-jwt-secret'),
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
+  },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+  transfer: {
+    token: {
+      salt: env('TRANSFER_TOKEN_SALT'),
     },
-    apiToken: {
-      salt: env('API_TOKEN_SALT', 'whatson-api-token-salt'),
-    },
-    transfer: {
-      token: {
-        salt: env('TRANSFER_TOKEN_SALT', 'whatson-transfer-token-salt'),
-      },
-    },
-  });
+  },
+});
--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,7 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'https://the37n.gr'),
+  url: env('PUBLIC_URL', 'https://the37n.gr'),
+  proxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
