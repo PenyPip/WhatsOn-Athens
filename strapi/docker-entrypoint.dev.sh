8dc5f36d3f78cc71489804e2d1cbcc3bd85255c5
PenyPip <penynikolaidou@gmail.com>
2026-06-03 18:50:17 +0300

    fix

--- strapi/docker-entrypoint.dev.sh
+++ strapi/docker-entrypoint.dev.sh
@@ -3,10 +3,13 @@ set -e
 cd /cms
 
 needs_ci=0
-if [ ! -d node_modules/@swc/core-linux-arm64-musl ] && [ ! -d node_modules/@swc/core-linux-x64-musl ]; then
+if [ ! -d node_modules/@swc/core-linux-arm64-gnu ] && [ ! -d node_modules/@swc/core-linux-x64-gnu ]; then
   needs_ci=1
 fi
 if [ -d node_modules/@swc/core-darwin-arm64 ] || [ -d node_modules/@swc/core-darwin-x64 ]; then
+  needs_ci=1
+fi
+if [ -d node_modules/@swc/core-linux-arm64-musl ] || [ -d node_modules/@swc/core-linux-x64-musl ]; then
   needs_ci=1
 fi
 
